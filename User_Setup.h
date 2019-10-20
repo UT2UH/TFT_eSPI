46600c0bdb80e91d012048373f69951e1f827423
px4ugs <px4ugs@users.noreply.github.com>
2019-10-20 21:20:11 +0300

    User Setup cleaned

--- User_Setup.h
+++ User_Setup.h
@@ -219,13 +219,9 @@
 #define TFT_DC   9   // Data Command control pin
 #define TFT_CS   10  // Chip select control pin
 #define TFT_RST  -1  // Set TFT_RST to -1 if display RESET is connected to STM32L0 board RST
-//#define TFT_RST   4  // Reset pin (could connect to RST pin)
+//#define TFT_BL   4  // LED back-light (only for board with MOSFET in front of backlight control pin)
 
-//#define TFT_BL   32  // LED back-light (only for ST7789 with backlight control pin)
-
-//#define TOUCH_CS 21     // Chip select pin (T_CS) of touch screen
-
-//#define TFT_WR 22    // Write strobe for modified Raspberry Pi TFT only
+//#define TOUCH_CS 6     // Chip select pin (T_CS) of touch screen
 
 
 // ##################################################################################
@@ -241,14 +237,14 @@
 
 #define LOAD_GLCD   // Font 1. Original Adafruit 8 pixel font needs ~1820 bytes in FLASH
 #define LOAD_FONT2  // Font 2. Small 16 pixel high font, needs ~3534 bytes in FLASH, 96 characters
-#if !defined (ARDUINO_ARCH_STM32L0)
-  #define LOAD_FONT4  // Font 4. Medium 26 pixel high font, needs ~5848 bytes in FLASH, 96 characters
-  #define LOAD_FONT6  // Font 6. Large 48 pixel font, needs ~2666 bytes in FLASH, only characters 1234567890:-.apm
-  #define LOAD_FONT7  // Font 7. 7 segment 48 pixel font, needs ~2438 bytes in FLASH, only characters 1234567890:-.
-  #define LOAD_FONT8  // Font 8. Large 75 pixel font needs ~3256 bytes in FLASH, only characters 1234567890:-.
-  //#define LOAD_FONT8N // Font 8. Alternative to Font 8 above, slightly narrower, so 3 digits fit a 160 pixel TFT
-  #define LOAD_GFXFF  // FreeFonts. Include access to the 48 Adafruit_GFX free fonts FF1 to FF48 and custom fonts
+#define LOAD_FONT4  // Font 4. Medium 26 pixel high font, needs ~5848 bytes in FLASH, 96 characters
+#define LOAD_FONT6  // Font 6. Large 48 pixel font, needs ~2666 bytes in FLASH, only characters 1234567890:-.apm
+#define LOAD_FONT7  // Font 7. 7 segment 48 pixel font, needs ~2438 bytes in FLASH, only characters 1234567890:-.
+#define LOAD_FONT8  // Font 8. Large 75 pixel font needs ~3256 bytes in FLASH, only characters 1234567890:-.
+//#define LOAD_FONT8N // Font 8. Alternative to Font 8 above, slightly narrower, so 3 digits fit a 160 pixel TFT
+#define LOAD_GFXFF  // FreeFonts. Include access to the 48 Adafruit_GFX free fonts FF1 to FF48 and custom fonts
 
+#if !defined (ARDUINO_ARCH_STM32L0)
   // Comment out the #define below to stop the SPIFFS filing system and smooth font code being loaded
   // this will save ~20kbytes of FLASH
   #define SMOOTH_FONT
